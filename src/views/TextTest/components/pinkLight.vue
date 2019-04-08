ee0700f09172e350d9e9f5324f62d5cb4cfbdba3
jerry000lin <1209815340@qq.com>
2019-04-08 14:42:08 +0800

    pinkLight

--- src/views/TextTest/components/pinkLight.vue
+++ src/views/TextTest/components/pinkLight.vue
@@ -10,13 +10,10 @@ export default {};
 <style lang="less">
 .title {
   font-family: "Montserrat";
-  position: relative;
-  color: #fff;
-  font-weight: 100;
   font-size: inherit;
-  padding: 0;
-  margin: 0;
+  color: #fff;
   line-height: 1;
+  font-weight: 100;
   text-shadow: 0 0 2px #ff006c, 0 0 4px #ff006c, 0 0 6px #ff006c,
     0 0 8px #ff417d, 0 0 14px #ff417d, 0 0 16px #ff417d, 0 0 20px #ff417d,
     0 0 30px #ff417d;
